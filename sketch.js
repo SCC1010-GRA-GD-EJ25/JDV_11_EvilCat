8a777b03227b47d1a952907eb581ceb864f2ff3e
23170373 <158121527+23170373@users.noreply.github.com>
2025-02-26 23:04:57 +0000

    Version 1

--- sketch.js
+++ sketch.js
@@ -1,14 +1,35 @@
+let gatito
+let gatito2
+
+
 function preload() {
   // put preload code here
+  gatito = loadImage('./images/gatito.jpg')
+  gatito2 = loadImage('./images/gatito.jpg')   
 }
 
 function setup() {
   // put setup code here
-  createCanvas(800,800)
+  createCanvas(gatito.width, gatito.height)
+  noLoop()
 }
 
 function draw() {
   // put drawing code here
   background(255)
-  rect(100,100,200,200)
+  let numPixeles = 4*gatito.width* gatito.height
+  gatito.loadPixels()
+  for (let i = 0; i < numPixeles; i+=4) {
+    //Red
+    gatito.pixels[i] = 255 - gatito.pixels[i]
+    //Green
+    gatito.pixels[i+1] = 255 - gatito.pixels[i+1]
+    //Blue
+    gatito.pixels[i+2] = 255 - gatito.pixels[i+2]
+    //Alpha
+    //gatito.pixels[i+3] = 255 - gatito.pixels[i+3]
+  }
+  gatito.updatePixels()
+  image(gatito, 0, 0)
+  image(gatito2, 0, 0, (gatito.width*0.2), (gatito.height*0.2))
 }
